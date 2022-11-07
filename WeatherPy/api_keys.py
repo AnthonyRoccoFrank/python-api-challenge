b5561f123b7e21bcc82fbcff8649f17aaf0da621
Anthony Rocco-Frank <106894159+AnthonyRoccoFrank@users.noreply.github.com>
2022-11-07 13:05:26 -0600

    Final

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "your key here"
+weather_api_key = "656ae079f77323762e67687b28c1ac8a"
 
 # Geoapify API Key
-google_key = "your key here"
+google_key = "AIzaSyCz7kvtLVQRDcI96Zy0TXI1eR22ZlkiIw8"
